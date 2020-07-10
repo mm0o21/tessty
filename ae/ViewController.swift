2cb3ede830615cf4656d8e5b5d5f512504835811
maoko <maomaofu0121@docomo.ne.jp>
2020-07-10 16:57:47 +0900

    Initial Commit

---
+++ ae/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ae
+//
+//  Created by Maoko Furuya on 2020/07/10.
+//  Copyright © 2020 Maoko Furuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
